06a0acdd6b4dc392b548456713a4565f4a39501c
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-23 18:09:06 -0300

    Campo de texto da página

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () =>  {
     return (
         //JSX
         <header className="banner">
@@ -9,5 +9,3 @@ function Banner() {
     )
 
 }
-
-export default Banner;
