bc531a8693f4b05a6d87471b537a6f6ac25c5537
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-26 12:59:15 -0300

    Ajuste onSubmit para form

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -20,8 +20,8 @@ const aoSalvar = (evento) => {
 
 const Formulario = () => {
     return (
-        <section onSubmit={aoSalvar} className='formulario'>
-            <form>
+        <section className='formulario'>
+            <form onSubmit={aoSalvar} >
                 <h2>Preencha os dados para criar o card do colaborador</h2>
                 <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo"/>
