7ee57e2832c2dd688baa8cf2b0c30e653eb120d8
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-26 20:19:24 -0300

    Controlando o estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,18 +1,24 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
-    
-    const labelModificada = `${props.label}`
-    const placeholderModificada = `${props.placeholder}...`
-    
-    return(
-        <div className="campo-texto">
-            <label>
-                {labelModificada}
-            </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
-        </div>
-    )
-}
+  const labelModificada = `${props.label}`;
+  const placeholderModificada = `${props.placeholder}...`;
+
+  const aoDigitado = (evento) => {
+    props.aoAlterado(evento.target.value);
+  };
+
+  return (
+    <div className="campo-texto">
+      <label>{labelModificada}</label>
+      <input
+        value={props.valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificada}
+      />
+    </div>
+  );
+};
 
 export default CampoTexto;
