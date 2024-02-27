93bd1aed544418cccce5973d729040a08e4caa26
Bruno Lins Maia <brunolinsm@gmail.com>
2024-02-27 16:26:19 -0300

    Card de colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/brunolinsm.png" alt="Bruno Lins" />
+      </div>
+      <div className="rodape">
+        <h4>Bruno Lins</h4>
+        <h5>Estudante</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) => {
@@ -7,6 +8,7 @@ const Time = (props) => {
   return (
     <section className="time" style={cssCorSecundaria}>
       <h3 style={cssCorPrimaria}>{props.nome}</h3>
+      <Colaborador />
     </section>
   );
 };
